e44d6241e3e57d59b9fa71c3e0508b932a60b2c1
Katumbela <ja3328173@gmail.com>
2023-06-16 13:06:17 +0100

    Update rotas.jsx

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -1,17 +1,21 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import Home from "./pages/inicial";
+// Importe os pacotes necessários
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// Importe os componentes das páginas
+import Home from './pages/inicial';
+// Importe outros componentes das páginas, se houver
 
-
+// Defina as rotas do aplicativo
 const RotasPT = () => {
-
-    return (
-        <BrowserRouter>
-                <Routes >
-                    <Route element={<Home/>} path="/" exact/>
-                </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Adicione outras rotas aqui */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default RotasPT;
